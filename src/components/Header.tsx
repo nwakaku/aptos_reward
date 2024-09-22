e1aff4d46862efafaf0d099bdc20b20dba0e8c47
sammy-code98 <sammysmart98@yahoo.com>
2024-09-22 15:40:25 +0100

    project scalfold

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,10 @@
 import { WalletSelector } from "./WalletSelector";
 
 export function Header() {
+  // max-w-screen-xl
   return (
-    <div className="flex items-center justify-between px-4 py-2 max-w-screen-xl mx-auto w-full flex-wrap">
-      <h1 className="display">Boilerplate Template</h1>
+    <div className="flex items-center justify-between px-4 py-3  mx-auto w-full flex-wrap shadow-md">
+      <h1 className="heading-sm">Aptos Reward</h1>
 
       <div className="flex gap-2 items-center flex-wrap">
         <WalletSelector />
